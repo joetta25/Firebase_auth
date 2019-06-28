15199652ed0dbf60c96de63d72cabea0422b6570
Juan Castillo <castillojuan1000@gmail.com>
2019-06-28 08:52:24 -0400

    SignUpPage: created a new css folder for signup.htm and implemented firebase to signUp.html/ new keys; created a SignIn.html and css for the same folder; the only modification i made to home.html was the link href:signUP.html

--- firebaseSignup.js
+++ firebaseSignup.js
@@ -1,24 +1,33 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+let submitForm = document.getElementById("signUpForm");
 
+submitForm.addEventListener("submit", e => {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+  let phoneNumber = document.getElementById("phone").value;
+  console.log(email, password);
 
-
-
-let submitForm = document.getElementById('signUpForm');
-submitForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let password = document.getElementById('pass').value
-    let phoneNumber = document.getElementById('phone').value
-    console.log(email, password)
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        document.getElementById('error').innerHTML = errorCode
-        console.log(errorCode)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      document.getElementById("error").innerHTML = errorCode;
+      console.log(errorCode);
     });
-})
+
+  document.getElementById("alert").style.display = "block";
+
+  setTimeout(() => {
+    document.getElementById("alert").style.display = "none";
+  }, 2000);
+
+  document.getElementById("signUpForm").reset();
+});
