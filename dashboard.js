213fa7a19bcb1a78459557fa8576113a9791d7ce
joetta25 <joetta.chasidy@gmail.com>
2019-07-02 19:10:30 -0400

    added next , previous , add buttons

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,11 @@
 //Spotify
 // Set up app config
+var page_count = 0;
+var next_url = '';
+var prev_url = '';
 var client_id = '57a5ae8651d746c3b9b1fcb24561af24';
 var response_type = 'token';
-var redirect_uri = 'http://127.0.0.1:5500/dashboard.html';
+var redirect_uri = 'http://127.0.0.1:5501/dashboard.html';
 var scope = [
     ''
 ].join(' ');
@@ -50,39 +53,62 @@ $('.search-form').on('submit', function(e) {
     // if the value is not empty
     if (value != '') {
         // get the search request from spotify
-        $.ajax(`https://api.spotify.com/v1/search?q=${value}&type=track`, {
+        $.ajax(`https://api.spotify.com/v1/search?q=${value}&type=track&limit=5`, {
             headers: {
                 'Authorization': `Bearer ${token}` // this is where we use the access_token
             }
         })
-        .then((data) => { // when the search request is finished // log the data to the console//console.log(data);
-            if (data.tracks.items.length > 0) {
-                var songsHTML = data.tracks.items.map(track => {
-                    var artistsString = track.artists.map(artist => {
-                        return artist.name;
-                    }).join(', ');
-                    return `
-                    <div class="card spotify-card col-3 mx-4 my-4 ">
-                        <img class="card-img-top" src="${track.album.images[0].url}" value= "${track.album.external_urls.spotify}"/>
-                        <div class="card-body">
-                        <h5 class="card-title">${artistsString} <span class="badge badge-secondary">${track.album.release_date}</span></h5>
-                        <h5 class="card-title">${track.name}</h5>
-                        <button class="btn btn-primary" type="button" onclick="favoriteSongs('${track.id}')">add</button>
-                        </div>
-                    </div>
-                    `
-                })
-                return songsHTML.join('');
-            } else {
-                return '<div>No results</div>'
-            }
-        })
-        .then((html) => {
-            document.getElementsByClassName('music-container-fluid')[0].innerHTML = html;
-        })
+        .then(renderSpotifyResults)
 
     }
 })
 
+function renderSpotifyResults(data) { // when the search request is finished // log the data to the console//console.log(data);
+    let html = '';
+    next_url = data.tracks.next;
+    prev_url = data.tracks.previous;
+    if (data.tracks.items.length > 0) {
+        var songsHTML = data.tracks.items.map(track => {
+            var artistsString = track.artists.map(artist => {
+                return artist.name;
+            }).join(', ');
+            return `
+                <div class = "album">
+                    <img  class = "mb-1" width = "50" src = ${track.album.images[0].url} value= "${track.album.external_urls.spotify}"/>
+                    <h3 class = "d-inline-block">${artistsString}</h3>
+                </div>
+                <div class = "song d-flex justify-content-between border-top border-bottom py-3">
+                    <b>${track.name}</b>
+                    <button type="button" data-spotify="${track.uri}" class="btn btn-primary">add</button>
+                </div>
+            `
+        })
+        html = songsHTML.join('');
+    } else {
+        html = '<div>No results</div>'
+    }
+
+    document.getElementsByClassName('music-container-fluid')[0].innerHTML = html;
+}
+
+$('.next').click((e) => {
+    e.preventDefault();
+    $.ajax(next_url, {
+        headers: {
+            'Authorization': `Bearer ${token}` // this is where we use the access_token
+        }
+    })
+    .then(renderSpotifyResults)
+});
+
+$('.previous').click((e)=> {
+    e.preventDefault();
+    $.ajax(prev_url, {
+        headers: {
+            'Authorization': `Bearer ${token}` // this is where we use the access_token
+        }
+    })
+    .then(renderSpotifyResults)
+})
 
 // Youtube
