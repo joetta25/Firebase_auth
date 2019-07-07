d26082515b29d8ac4efbeeb7db5df1e4c3aead02
Mike Le <mike@Mikes-MBP.attlocal.net>
2019-07-06 22:44:58 -0400

    added function to convert ISO8601 duration to HH:MM:SS format

--- dashboard.js
+++ dashboard.js
@@ -236,7 +236,35 @@ function makeRequest(token) {
                 })
 
                 detailsRequest.execute(function(details) {
-                    var videoDuration = details.items[0].contentDetails.duration;
+                  var videoDuration = ISO8601toDuration(details.items[0].contentDetails.duration);
+
+                  // Check index of returned YT ISO8601 time format and trim
+                  function formatTimeUnit(input, unit){
+                      var index = input.indexOf(unit);
+                      var output = "00"
+                      if (index < 0) {
+                          return output; // unit isn't in the input
+                      }
+                      if (isNaN(input.charAt(index-2))) {
+                          return '0' + input.charAt(index-1);
+                      } else {
+                          return input.charAt(index-2) + input.charAt(index-1);
+                      }
+                  }
+
+                  // Convert ISO8601 format to time HH:MM:SS
+                  function ISO8601toDuration(input){
+                      var H = formatTimeUnit(input, 'H');
+                      var M = formatTimeUnit(input, 'M');
+                      var S = formatTimeUnit(input, 'S');
+                      if (H == "00") {
+                          H = "";
+                      } else {
+                          H += ":"
+                      }
+                  
+                      return H  + M + ':' + S ;
+                  }
 
                     $videoContainer.append(`
                         <div class="container">
