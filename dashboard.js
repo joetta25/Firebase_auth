76a8aaf7fb1380f04283f4502bebf87b04314755
joetta25 <joetta.chasidy@gmail.com>
2019-07-09 20:59:51 -0400

    added buttons and styled music container

--- dashboard.js
+++ dashboard.js
@@ -23,6 +23,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 //Spotify
 // Set up app config
+var page_count = 0;
+var next_url = '';
+var prev_url = '';
 var client_id = "57a5ae8651d746c3b9b1fcb24561af24";
 var response_type = "token";
 var redirect_uri = "http://127.0.0.1:5501/dashboard.html";
@@ -64,228 +67,232 @@ if (token !== null) {
 }
 
 // when the form is submitted
-$(".search-form").on("submit", function(e) {
+$('.search-form').on('submit', function(e) {
   e.preventDefault();
   // get the search value from the search field
-  var value = $("#searchname").val();
+  var value = $('#searchname').val();
   // if the value is not empty
-  if (value != "") {
-    // get the search request from spotify
-    $.ajax(`https://api.spotify.com/v1/search?q=${value}&type=track`, {
+  if (value != '') {
+      // get the search request from spotify
+    $.ajax(`https://api.spotify.com/v1/search?q=${value}&type=track&limit=5`,{
       headers: {
-        Authorization: `Bearer ${token}` // this is where we use the access_token
+              'Authorization': `Bearer ${token}` // this is where we use the access_token
+          }
+        
+      })
+      .then(renderSpotifyResults)
+  }
+})
+function renderSpotifyResults(data) { // when the search request is finished // log the data to the console//console.log(data);
+  let html = '';
+  next_url = data.tracks.next;
+  prev_url = data.tracks.previous;
+  if (data.tracks.items.length > 0) {
+      var songsHTML = data.tracks.items.map(track => {
+        var artistsString = track.artists.map(artist => {
+          return artist.name;
+        }).join(', ');    
+        return `
+          <div class = "album">
+            <img  class = "mb-1" width = "50" src = ${track.album.images[0].url} value= "${track.album.external_urls.spotify}"/>
+            <h3 class = "d-inline-block">${artistsString}</h3>
+          </div>
+          <div class = "song d-flex justify-content-between border-top border-bottom py-3">
+            <b>${track.name}</b>
+            <button type="button" data-spotify="${track.uri}" class="btn btn-primary">add</button>
+          </div>
+                `
+      })
+      html = songsHTML.join('');
+    } else {
+      html = '<div>No results</div>'
+    }
+    document.getElementsByClassName('music-container-fluid')[0].innerHTML = html;
+}
+
+$('.next').click((e) => {
+    e.preventDefault();
+    $.ajax(next_url, {
+      headers: {
+        'Authorization': `Bearer ${token}` // this is where we use the access_token
       }
     })
-      .then(data => {
-        // when the search request is finished // log the data to the console//console.log(data);
-        if (data.tracks.items.length > 0) {
-          var songsHTML = data.tracks.items.map(track => {
-            var artistsString = track.artists
-              .map(artist => {
-                return artist.name;
-              })
-              .join(", ");
-            return `
-                    <div class="card spotify-card col-3 mx-4 my-4 ">
-                        <img class="card-img-top" src="${
-                          track.album.images[0].url
-                        }" value= "${track.album.external_urls.spotify}"/>
-                        <div class="card-body">
-                        <h5 class="card-title">${artistsString} <span class="badge badge-secondary">${
-              track.album.release_date
-            }</span></h5>
-                        <h5 class="card-title">${track.name}</h5>
-                        <button class="btn btn-primary" type="button" onclick="favoriteSongs('${
-                          track.id
-                        }')">add</button>
-                        </div>
-                    </div>
-                    `;
-          });
-          return songsHTML.join("");
-        } else {
-          return "<div>No results</div>";
-        }
-      })
-      .then(html => {
-        document.getElementsByClassName(
-          "music-container-fluid"
-        )[0].innerHTML = html;
-      });
-  }
-});
-
-// Youtube
-// Use localhost port 8888
-// const ytApiKey = "AIzaSyDHaIMUkv2DdX8RqP0rmf8QIhcCg_5KU08"; API is over quota limit
-const ytApiKey = "AIzaSyCw7Gw6BDPrcRiqjfTMfDckh_11BKWl7HM"
-const ytCLIENT_ID = "254484771306-7r6u8p6efbpqcjru9diqqnq4naoval1o.apps.googleusercontent.com";
-const ytDISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
-const ytSCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
-
-const authorizeButton = document.getElementById('authorize-button');
-const signoutButton = document.getElementById('signout-button');
-const content = document.getElementById('content');
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
-const videoContainer = document.getElementById('video-container');
-const searchTerm = document.querySelector('search-input');
+    .then(renderSpotifyResults)
+  });
 
 
-// Form submit 
-searchForm.addEventListener('submit', e => {
-    e.preventDefault();
 
-    let search = searchInput.value;
-    makeRequest();
-});
+// // Youtube
+// // Use localhost port 8888
+// // const ytApiKey = "AIzaSyDHaIMUkv2DdX8RqP0rmf8QIhcCg_5KU08"; API is over quota limit
+// const ytApiKey = "AIzaSyCw7Gw6BDPrcRiqjfTMfDckh_11BKWl7HM"
+// const ytCLIENT_ID = "254484771306-7r6u8p6efbpqcjru9diqqnq4naoval1o.apps.googleusercontent.com";
+// const ytDISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
+// const ytSCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-// Load auth2 library
-function handleClientLoad() {
-gapi.load('client:auth2', initClient);
-}
+// const authorizeButton = document.getElementById('authorize-button');
+// const signoutButton = document.getElementById('signout-button');
+// const content = document.getElementById('content');
+// const searchForm = document.getElementById('search-form');
+// const searchInput = document.getElementById('search-input');
+// const videoContainer = document.getElementById('video-container');
+// const searchTerm = document.querySelector('search-input');
 
-// Init API client
-function initClient() {
-gapi.client
-    .init({
-    discoveryDocs: ytDISCOVERY_DOCS,
-    clientId: ytCLIENT_ID,
-    scope: ytSCOPES
-    })
-    .then(() => {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-    });
-}
 
-// Hide and unhide elements based on login state
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        content.style.display = 'block';
-        videoContainer.style.display = 'block';
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-        content.style.display = 'none';
-        videoContainer.style.display = 'none';
-    }
-}
+// // Form submit 
+// searchForm.addEventListener('submit', e => {
+//     e.preventDefault();
 
-// Login and Logout
-function handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn();
-}
-function handleSignoutClick() {
-    gapi.auth2.getAuthInstance().signOut();
-}
+//     let search = searchInput.value;
+//     makeRequest();
+// });
 
-function searchList(){
-    gapi.client.setApiKey(ytApiKey); 
-    gapi.client.load('youtube', 'v3', function() {
-        makeRequest();
-    });
-}
+// // Load auth2 library
+// function handleClientLoad() {
+// gapi.load('client:auth2', initClient);
+// }
 
-function makeRequest(token) {
-    var q = $('#search-input').val().toLowerCase();
+// // Init API client
+// function initClient() {
+// gapi.client
+//     .init({
+//     discoveryDocs: ytDISCOVERY_DOCS,
+//     clientId: ytCLIENT_ID,
+//     scope: ytSCOPES
+//     })
+//     .then(() => {
+//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+//     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//     authorizeButton.onclick = handleAuthClick;
+//     signoutButton.onclick = handleSignoutClick;
+//     });
+// }
+
+// // Hide and unhide elements based on login state
+// function updateSigninStatus(isSignedIn) {
+//     if (isSignedIn) {
+//         authorizeButton.style.display = 'none';
+//         signoutButton.style.display = 'block';
+//         content.style.display = 'block';
+//         videoContainer.style.display = 'block';
+//     } else {
+//         authorizeButton.style.display = 'block';
+//         signoutButton.style.display = 'none';
+//         content.style.display = 'none';
+//         videoContainer.style.display = 'none';
+//     }
+// }
+
+// // Login and Logout
+// function handleAuthClick() {
+//     gapi.auth2.getAuthInstance().signIn();
+// }
+// function handleSignoutClick() {
+//     gapi.auth2.getAuthInstance().signOut();
+// }
+
+// function searchList(){
+//     gapi.client.setApiKey(ytApiKey); 
+//     gapi.client.load('youtube', 'v3', function() {
+//         makeRequest();
+//     });
+// }
+
+// function makeRequest(token) {
+//     var q = $('#search-input').val().toLowerCase();
     
-    // make API search request
-    var searchRequest = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet', 
-        type: 'video',
-        maxResults: 5,
-        pageToken: token 
-    })
+//     // make API search request
+//     var searchRequest = gapi.client.youtube.search.list({
+//         q: q,
+//         part: 'snippet', 
+//         type: 'video',
+//         maxResults: 5,
+//         pageToken: token 
+//     })
 
-    searchRequest.execute(function(response)  {                                                                                    
-            $('#video-container').empty()
-            var srchItems = response.result.items; 
+//     searchRequest.execute(function(response)  {                                                                                    
+//             $('#video-container').empty()
+//             var srchItems = response.result.items; 
             
-            // Global variables
-            window.nextPageToken = response.nextPageToken;
-            window.prevPageToken = response.prevPageToken;
+//             // Global variables
+//             window.nextPageToken = response.nextPageToken;
+//             window.prevPageToken = response.prevPageToken;
             
-            var $videoContainer = $('#video-container')
+//             var $videoContainer = $('#video-container')
             
-            if(token) {
-                $videoContainer.html("");
-            }
+//             if(token) {
+//                 $videoContainer.html("");
+//             }
 
-            // toggleClass accepts 2 arguments (1st is required, 2nd one is optional)
-            // if you specify a 2nd argument, the class would be added or removed depending on the boolean
-            $("#prev").toggleClass("hide", !window.prevPageToken)
-            $("#next").toggleClass("hide", !window.nextPageToken)
+//             // toggleClass accepts 2 arguments (1st is required, 2nd one is optional)
+//             // if you specify a 2nd argument, the class would be added or removed depending on the boolean
+//             $("#prev").toggleClass("hide", !window.prevPageToken)
+//             $("#next").toggleClass("hide", !window.nextPageToken)
             
-            $.each(srchItems, function(index, item) {
-                var vidTitle = item.snippet.title;  
-                var videoId = item.id.videoId;
-                var videoImg = item.snippet.thumbnails.default.url;  
+//             $.each(srchItems, function(index, item) {
+//                 var vidTitle = item.snippet.title;  
+//                 var videoId = item.id.videoId;
+//                 var videoImg = item.snippet.thumbnails.default.url;  
                 
-                // make API video request in order to get duration
-                var detailsRequest = gapi.client.youtube.videos.list({
-                    id: videoId,
-                    part: 'contentDetails', 
-                    type: 'video',
-                    maxResults: 5
-                })
+//                 // make API video request in order to get duration
+//                 var detailsRequest = gapi.client.youtube.videos.list({
+//                     id: videoId,
+//                     part: 'contentDetails', 
+//                     type: 'video',
+//                     maxResults: 5
+//                 })
 
-                detailsRequest.execute(function(details) {
-                  var videoDuration = ISO8601toDuration(details.items[0].contentDetails.duration);
+//                 detailsRequest.execute(function(details) {
+//                   var videoDuration = ISO8601toDuration(details.items[0].contentDetails.duration);
 
-                  // Check index of returned YT ISO8601 time format and trim
-                  function formatTimeUnit(input, unit){
-                      var index = input.indexOf(unit);
-                      var output = "00"
-                      if (index < 0) {
-                          return output; // unit isn't in the input
-                      }
-                      if (isNaN(input.charAt(index-2))) {
-                          return '0' + input.charAt(index-1);
-                      } else {
-                          return input.charAt(index-2) + input.charAt(index-1);
-                      }
-                  }
+//                   // Check index of returned YT ISO8601 time format and trim
+//                   function formatTimeUnit(input, unit){
+//                       var index = input.indexOf(unit);
+//                       var output = "00"
+//                       if (index < 0) {
+//                           return output; // unit isn't in the input
+//                       }
+//                       if (isNaN(input.charAt(index-2))) {
+//                           return '0' + input.charAt(index-1);
+//                       } else {
+//                           return input.charAt(index-2) + input.charAt(index-1);
+//                       }
+//                   }
 
-                  // Convert ISO8601 format to time HH:MM:SS
-                  function ISO8601toDuration(input){
-                      var H = formatTimeUnit(input, 'H');
-                      var M = formatTimeUnit(input, 'M');
-                      var S = formatTimeUnit(input, 'S');
-                      if (H == "00") {
-                          H = "";
-                      } else {
-                          H += ":"
-                      }
+//                   // Convert ISO8601 format to time HH:MM:SS
+//                   function ISO8601toDuration(input){
+//                       var H = formatTimeUnit(input, 'H');
+//                       var M = formatTimeUnit(input, 'M');
+//                       var S = formatTimeUnit(input, 'S');
+//                       if (H == "00") {
+//                           H = "";
+//                       } else {
+//                           H += ":"
+//                       }
                   
-                      return H  + M + ':' + S ;
-                  }
+//                       return H  + M + ':' + S ;
+//                   }
 
-                    $videoContainer.append(`
-                        <div class="container">
-                            <img src="${videoImg}"/>
-                            ${vidTitle}
-                            ${videoDuration}
-                            <button src="https://www.youtube.com/watch?v=${videoId}">Add</button>
-                            ${videoId}
-                        </div>`);    
-                })
-            })
-    })
-}
+//                     $videoContainer.append(`
+//                         <div class="container">
+//                             <img src="${videoImg}"/>
+//                             ${vidTitle}
+//                             ${videoDuration}
+//                             <button src="https://www.youtube.com/watch?v=${videoId}">Add</button>
+//                             ${videoId}
+//                         </div>`);    
+//                 })
+//             })
+//     })
+// }
 
-function getNext () {
-    makeRequest(window.nextPageToken);
-}
+// function getNext () {
+//     makeRequest(window.nextPageToken);
+// }
 
-function getPrev () {
-    makeRequest(window.prevPageToken);
-}
+// function getPrev () {
+//     makeRequest(window.prevPageToken);
+// }
 
-document.getElementById("next").addEventListener("click", getNext)
-document.getElementById("prev").addEventListener("click", getPrev)
+// document.getElementById("next").addEventListener("click", getNext)
+// document.getElementById("prev").addEventListener("click", getPrev)
